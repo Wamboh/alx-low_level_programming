42c94b632910117761be89f23ba10e918f3bfd74
Maureen wairimu <maureenwairimi17@gmail.com>
2023-02-16 07:26:59 -0800

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
