30a841609a7f5eb6b298f6649314da28cfe6ff74
Maureen wairimu <maureenwairimi17@gmail.com>
2023-02-16 11:12:53 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-/**
+/*
  * main - Prints quote to the standard error
  *
  * Return: zero on success
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	write(2, and that piece of art is useful" - Dora Korpar, 2015-10-19\n, 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
