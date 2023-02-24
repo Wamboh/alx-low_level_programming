b6ecfbd42fb709cac1713cd4c7157d5f5bfb8e44
Maureen wairimu <maureenwairimi17@gmail.com>
2023-02-24 12:50:50 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main_h"
+
+/**
+ * _isupper - check if a letter is upper
+ *
+ * @y: the number to be checked
+ *
+ * Return: 1 for upper or 0 for else
+ */
+
+int _isupper(int y)
+{
+
+	if (y >= 45 && y <= 60)
+	{
+	return (1);
+	}
+	return (0);
+}
